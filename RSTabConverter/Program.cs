f4b446333a28fc6cb2bb6c4a53ae7d5752d6051a
Holger Frydrych <holger@frydrych.org>
2014-08-01 23:56:10 +0200

    First steps toward creating song index

--- RSTabConverter/Program.cs
+++ RSTabConverter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CommandLine;
 using CommandLine.Text;
+using RSTabConverterLib;
 
 namespace RSTabConverter
 {
@@ -13,6 +14,19 @@ namespace RSTabConverter
             if (CommandLine.Parser.Default.ParseArguments(args, options))
             {
                 Console.WriteLine("Opening archive {0} ...", options.PsarcFile);
+                try
+                {
+                    var browser = new PsarcBrowser(options.PsarcFile);
+                    var trackList = browser.GetTrackList();
+                    foreach (var track in trackList)
+                    {
+                        Console.WriteLine("{0} - {1}  [{2}, {3}]", track.Artist, track.Title, track.Album, track.Year);
+                    }
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    Console.WriteLine("The specified psarc file does not exist: {0}", e.FileName);
+                }
             }
         }
     }
--- RSTabConverterLib/PSARCBrowser.cs
+++ RSTabConverterLib/PSARCBrowser.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RocksmithToolkitLib;
+using RocksmithToolkitLib.PSARC;
+
 
 namespace RSTabConverterLib
 {
-    public class PSARCBrowser
+    /// <summary>
+    /// Reads in a Rocksmith PSARC archive and collects information on the 
+    /// contained tracks. Can extract specific tracks for processing.
+    /// </summary>
+    public class PsarcBrowser
     {
+        private PSARC archive;
+
+        /// <summary>
+        /// Create a new PsarcBrowser from a specified archive file.
+        /// </summary>
+        /// <param name="fileName">Path of the .psarc file to open.</param>
+        public PsarcBrowser(string fileName)
+        {
+            archive = new PSARC();
+            using (var stream = File.OpenRead(fileName))
+            {
+                archive.Read(stream);
+            }
+        }
+
+        public IList<TrackInfo> GetTrackList()
+        {
+            // Each song has a corresponding .json file within the archive containing
+            // information about it.
+            var infoFiles = archive.Entries.Where(x => x.Name.StartsWith(@"manifests/songs")
+                && x.Name.EndsWith(".json")).OrderBy(x => x.Name);
+
+            var trackList = new List<TrackInfo>();
+
+            foreach (var entry in infoFiles)
+            {
+                Console.WriteLine("Entry found: {0}", entry.Name);
+                using (var reader = new StreamReader(entry.Data))
+                {
+                    JObject o = JObject.Parse(reader.ReadToEnd());
+                    var attributes = o["Entries"].First.Last["Attributes"];
+                    var title = attributes["SongName"].ToString();
+                    var artist = attributes["ArtistName"].ToString();
+                    var album = attributes["AlbumName"].ToString();
+                    var year = attributes["SongYear"].ToString();
+
+                    var info = new TrackInfo()
+                    {
+                        Title = attributes["SongName"].ToString(),
+                        Artist = attributes["ArtistName"].ToString(),
+                        Album = attributes["AlbumName"].ToString(),
+                        Year = attributes["SongYear"].ToString()
+                    };
+                    trackList.Add(info);
+                }                
+            }
+
+            return trackList;
+        }
+    }
+
+
+    public class TrackInfo
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Year { get; set; }
+        public string Key { get; set; }
     }
 }
