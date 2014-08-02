9f0936100db9c276c78f5904527eadf087a7b15e
Holger Frydrych <holger@frydrych.org>
2014-08-02 17:15:19 +0200

    Preparations for export

---
+++ RSTabConverterLib/MusicXmlExporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RocksmithToolkitLib.Xml;
+
+namespace RSTabConverterLib
+{
+    public class MusicXmlExporter
+    {
+        private Song2014 song;
+
+        public MusicXmlExporter(Song2014 song)
+        {
+            this.song = song;
+        }
+    }
+}
--- RSTabConverterLib/PSARCBrowser.cs
+++ RSTabConverterLib/PSARCBrowser.cs
@@ -8,6 +8,10 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RocksmithToolkitLib;
 using RocksmithToolkitLib.PSARC;
+using RocksmithToolkitLib.Xml;
+using RocksmithToolkitLib.DLCPackage.Manifest;
+using RocksmithToolkitLib.DLCPackage;
+using RocksmithToolkitLib.Sng2014HSL;
 
 
 namespace RSTabConverterLib
@@ -19,6 +23,7 @@ namespace RSTabConverterLib
     public class PsarcBrowser
     {
         private PSARC archive;
+        private Platform platform;
 
         /// <summary>
         /// Create a new PsarcBrowser from a specified archive file.
@@ -27,6 +32,7 @@ namespace RSTabConverterLib
         public PsarcBrowser(string fileName)
         {
             archive = new PSARC();
+            platform = fileName.GetPlatform();
             using (var stream = File.OpenRead(fileName))
             {
                 archive.Read(stream);
@@ -61,7 +67,7 @@ namespace RSTabConverterLib
                 if (currentTrack == null || currentTrack.Identifier != identifier)
                 {
                     // extract track info from the .json file
-                    using (var reader = new StreamReader(entry.Data))
+                    using (var reader = new StreamReader(entry.Data, new UTF8Encoding(), false, 1024, true))
                     {
                         try
                         {
@@ -95,6 +101,41 @@ namespace RSTabConverterLib
             }
 
             return trackList;
+        }
+
+
+        /// <summary>
+        /// Extract a particular arrangement of a track from the archive
+        /// and return it in its Xml representation as a Song2014 object.
+        /// </summary>
+        /// <param name="identifier">Identifier of the track to load.</param>
+        /// <param name="arrangement">The arrangement to use.</param>
+        /// <returns>A Song2014 object containing the arrangement.</returns>
+        public Song2014 GetArrangement(string identifier, string arrangement)
+        {
+            // In order to instantiate a Rocksmith Song2014 object, we need both
+            // the binary .sng file and the attributes contained in the corresponding
+            // .json manifest.
+            var sngFile = archive.Entries.FirstOrDefault(x => x.Name == "songs/bin/generic/" +
+                identifier + "_" + arrangement + ".sng");
+            var jsonFile = archive.Entries.FirstOrDefault(x => x.Name.StartsWith("manifest/songs") &&
+                x.Name.EndsWith("/" + identifier + "_" + arrangement + ".json"));
+            if (sngFile == null || jsonFile == null)
+                return null;
+
+            // read out attributes from .json manifest
+            Attributes2014 attr;
+            using (var reader = new StreamReader(jsonFile.Data, new UTF8Encoding(), false, 1024, true))
+            {
+                var manifest = JsonConvert.DeserializeObject<Manifest2014<Attributes2014>>(
+                    reader.ReadToEnd());
+                attr = manifest.Entries.ToArray()[0].Value.ToArray()[0].Value;
+            }
+
+            // get contents of .sng file
+            Sng2014File sng = Sng2014File.ReadSng(sngFile.Data, platform);
+
+            return new Song2014(sng, attr);
         }
     }
 
